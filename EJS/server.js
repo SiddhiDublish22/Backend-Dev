72eebbac2d91c2d10171787bd45667cca7e16ad3
SiddhiDublish22 <siddhidublish.22@gmail.com>
2026-02-03 15:22:14 +0530

    all tasks of EJS

---
+++ EJS/server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const app = express();
+
+app.set("view engine" , "ejs");
+
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+
+app.get("/user",(req,res)=>{
+    let userData = {
+        name:"Siddhi",
+        age: "20"
+    }
+    res.render("user",{userData});
+})
+
+app.get("/list",(req,res)=>{
+
+    let arr = ["apple","mango","orange"]
+
+    res.render("list",{arr});
+})
+
+app.listen(3000,()=>{
+    console.log("server is running");
+})
